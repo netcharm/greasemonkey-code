a94c141f8e5a5ebf17456020ecf435dc2e19462f
netcharm <netcharm@163.com>
2017-12-03 22:39:24 +0800

    1. fixed minor bugs for markdown to HTML/PDF 2. change pandoc options for pandoc 2.0.3

--- md2pdf.cmd
+++ md2pdf.cmd
@@ -6,8 +6,8 @@ set PANDOC_DATA=%PANDOC_PATH%\templates
 set PANDOCCITE=%PANDOC_PATH%\pandoc-citeproc.exe
 set PANDOCOPT_META=-M 'papersize:a4' -M 'lang:zh-CN'
 set PANDOCOPT_VAR=-V 'papersize:a4' -V 'lang:zh-CN'
-set MARKDOWN_FLAG=markdown+fenced_code_blocks+raw_html+pipe_tables+backtick_code_blocks+auto_identifiers+ascii_identifiers+shortcut_reference_links+markdown_in_html_blocks+yaml_metadata_block+inline_code_attributes+tex_math_dollars
-set PANDOCOPT="%~dp0pdf-template.yaml" --data-dir=%PANDOC_DATA% --smart --dpi=600 -f %MARKDOWN_FLAG% -t latex -s --toc --latex-engine=xelatex --template="%~dp0pdf-template.tex"
+set MARKDOWN_FLAG=markdown+fenced_code_blocks+raw_html+pipe_tables+backtick_code_blocks+auto_identifiers+ascii_identifiers+shortcut_reference_links+markdown_in_html_blocks+inline_code_attributes+tex_math_dollars+yaml_metadata_block+smart
+set PANDOCOPT="%~dp0pdf-template.yaml" --data-dir=%PANDOC_DATA% --dpi=600 -f %MARKDOWN_FLAG% -t latex -s --toc --pdf-engine=xelatex --template="%~dp0pdf-template.tex"
 rem %PANDOCOPT_META% %PANDOCOPT_META%
 set PANDOCOPTC=%PANDOCOPT% --filter %PANDOCCITE%
 goto START
